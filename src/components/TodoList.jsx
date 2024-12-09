5acc8163ff06a8632f4fb43dde6bea60010496f2
udita <uditagowdety@gmail.com>
2024-12-09 11:47:15 +0530

    made the list mapping work

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export default function TodoList() {
+    let todos=[
+        'go to shopping mall',
+        'call mom',
+        'read a book',
+        'water plants'
+    ]
+    
   return (
-    <div>TodoList</div>
+    <ul>
+        {todos.map((todo,todoIndex)=>{
+            return (
+                <li key={todoIndex}>{todo}</li>
+            )
+        })}
+    </ul>
   )
 }
