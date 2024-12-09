e9e9586b880e5060ef0636ca6917b0555a82db48
udita <uditagowdety@gmail.com>
2024-12-09 12:01:42 +0530

    rendered the todo card functionality

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function TodoCard() {
-  return (
-    <div>TodoCard</div>
-  )
+export default function TodoCard(props) {
+    const {children}=props
+    return (
+    <li className='todoItem'>
+        {children}
+        <div className='actionsContainer'>
+            <i className="fa-regular fa-pen-to-square"></i>
+            <i className="fa-regular fa-trash-can"></i>
+        </div>
+    </li>
+    )
 }
