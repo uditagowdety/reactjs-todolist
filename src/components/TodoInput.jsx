f69884b6b2f21ea565085fe1ad9ca0b1f6748d98
udita <uditagowdety@gmail.com>
2024-12-09 12:36:06 +0530

    input resets

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -10,6 +10,7 @@ export default function TodoInput(props){
             }} placeholder='Enter todo...'/>
             <button onClick={()=>{
                 handleAddTodos(todoValue)
+                setTodoValue('')
             }}>Add</button>
         </header>
     )
